dec24ad246d81339a3f3722297f29a023c21b04b
wamalias <111508713+wamalias@users.noreply.github.com>
2023-06-12 10:32:47 +0700

    Create 3.sql

---
+++ warda/3.sql
@@ -0,0 +1,8 @@
+-- Menampilkan 20 nama depan dan nama belakang sebagai nama pelanggan dengan provinsi pelanggan adalah 'Oklahoma' diurutkan berdasarkan jumlah transaksi. Jika ada jumlah transaksi yang sama, urutkan berdasarkan abjad nama pelanggan
+
+SELECT p.Nama_Depan ||' '|| p.Nama_belakang AS nama_pelanggan, COUNT(t.id) AS jumlah_transaksi FROM Pelanggan p
+INNER JOIN transaksi t ON t.pelanggan_id = p.id
+WHERE p.negara = 'New Zealand'
+GROUP BY p.Nama_Depan, p.Nama_Belakang
+ORDER BY jumlah_transaksi DESC, nama_pelanggan ASC
+LIMIT 20;
