cb8cd044c2b76e7783a7e5d0fb45ce6822c329d8
Hammuda Arsyad <98505313+H-mD@users.noreply.github.com>
2023-06-12 04:37:31 +0000

    fix: Hammuda/procedure.sql

--- Hammuda/procedure.sql
+++ Hammuda/procedure.sql
@@ -1,19 +1,12 @@
 CREATE OR REPLACE PROCEDURE pecat(id_pegawai INT)
 LANGUAGE plpgsql    
 AS $$
-DECLARE
-	id_atasan INT;
 BEGIN
-	SELECT p.atasan INTO id_atasan
-	FROM pegawai
-    WHERE p.id = id_pegawai;
-	
-	IF id_atasan IS NOT NULL THEN
-    	UPDATE pegawai
-	    SET atasan = id_atasan
-    	WHERE atasan = id_pegawai;
-    	COMMIT;
-	ELSE
-		RAISE EXCEPTION 'cannot fire owner level employee';
-	END IF;
+    UPDATE pegawai
+	SET atasan = NULL
+    WHERE atasan = id_pegawai;
+
+	DELETE FROM pegawai
+	WHERE id = id_pegawai;
+    COMMIT;
 END;$$
