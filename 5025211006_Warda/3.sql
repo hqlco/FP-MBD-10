a21270e087515ea5074371d8fade059b8836c068
wamalias <111508713+wamalias@users.noreply.github.com>
2023-06-12 12:09:47 +0700

    Update 3.sql

--- 5025211006_Warda/3.sql
+++ 5025211006_Warda/3.sql
@@ -1,4 +1,5 @@
--- Menampilkan 20 nama depan dan nama belakang sebagai nama pelanggan dengan provinsi pelanggan adalah 'Oklahoma' diurutkan berdasarkan jumlah transaksi. Jika ada jumlah transaksi yang sama, urutkan berdasarkan abjad nama pelanggan
+-- Menampilkan 20 nama depan dan nama belakang sebagai nama pelanggan dengan negara asal pelanggan adalah 'New Zealand' diurutkan berdasarkan jumlah transaksi. 
+--Jika ada jumlah transaksi yang sama, urutkan berdasarkan abjad nama pelanggan
 
 SELECT p.Nama_Depan ||' '|| p.Nama_belakang AS nama_pelanggan, COUNT(t.id) AS jumlah_transaksi FROM Pelanggan p
 INNER JOIN transaksi t ON t.pelanggan_id = p.id
